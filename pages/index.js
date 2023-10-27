56ce024710e9a91542cfc31f3fe2e12f492cad97
Roger Graham <roger.graham@tuni.fi>
2023-10-27 17:08:25 +0300

    Change to use api-util to fetch api data on the server

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import EventList from '../components/events/event-list';
-import { getFeaturedEvents } from '../dummy-data'
+import { getFeaturedEvents } from "../helpers/api-util";
+import EventList from "../components/events/EventList";
 
-function HomePage() {
-
-  const featuredEvents= getFeaturedEvents();
-
+function HomePage(props) {
   return (
     <div>
-     <ul><EventList items={featuredEvents}/></ul>
+      <EventList items={props.events} />
     </div>
-  )
+  );
 }
 
-export default HomePage
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
+}
+
+export default HomePage;
