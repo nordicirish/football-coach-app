4b20df6cbf97c2098d66b7c77387a63557b6725f
Roger Graham <roger.graham@tuni.fi>
2023-10-27 17:02:53 +0300

    Add api-util function sto fetch and filter data from firebase endpoint

---
+++ helpers/api-util.js
@@ -0,0 +1,55 @@
+export async function getAllEvents() {
+  const response = await fetch(
+    "https://football-coach-app-402512-default-rtdb.europe-west1.firebasedatabase.app/events.json"
+  );
+  const data = await response.json();
+  console.log("[api-util] data", data);
+
+  const events = [];
+  // create an array of objects from the data
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+  console.log("[api-util] events", events);
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured);
+}
+
+export async function getEventById(id) {
+  const allEvents = await getAllEvents();
+  return allEvents.find((event) => event.id === id);
+}
+
+export async function getFilteredEvents(dateFilter) {
+  const allEvents = await getAllEvents();
+  const { year, month } = dateFilter;
+
+  let filteredEvents = allEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
+  return filteredEvents;
+}
+
+// move to helpers/date-util.js
+export async function getHumanReadableDate(dateString) {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("fi-Fi", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: false,
+  });
+}
